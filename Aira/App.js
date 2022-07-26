bd8a9647fca5213c3eca86875dfd6d94f5478dbc
Leonardo Gomes Da Silva <35742346+LeonardoGomes99@users.noreply.github.com>
2022-07-26 20:11:33 -0300

    formatando app

--- Aira/App.js
+++ Aira/App.js
@@ -19,55 +19,52 @@ import MessagesBox from './src/components/body/messageBox';
 export default function App() {
 
   const [messages, setMessages] = useState([]);
-  const [dicionario, setDicionario] = useState({ "resposta" : {
-    "Oi" : "Olá!",
-    "Como vai" : "Vou bem, obrigada!",
-    "Você está" : "Estou bem.",
+  const [dicionario, setDicionario] = useState({
+    "resposta": {
+      "Oi": "Olá!",
+      "Como vai": "Vou bem, obrigada!",
+      "Você está": "Estou bem.",
     },
   });
 
-    function sendMessage(message) {   
-      if(!message){
-        return;
-      }
-
-      let Message = (<View style={styles.body_chat_main_container_message_me}><Text style={styles.body_chat_main_container_chat_message_me}>{message}</Text></View>);
-      let AiraAnswer = AiraChooseAnswer(message);
-
-      setMessages(prevState => [...prevState, Message, AiraAnswer]);
-      // setTextInput('');
+  function sendMessage(message) {
+    if (!message) {
+      return;
     }
 
-    function AiraChooseAnswer(MessageFromUser) {
-      let AiraAnswer = (<View style={styles.body_chat_main_container_message_aira}><Text style={styles.body_chat_main_container_chat_message_aira}>{dicionario['resposta'][MessageFromUser]}</Text></View>);
-      return AiraAnswer;
-    }
+    let Message = (<View style={styles.body_chat_main_container_message_me}><Text style={styles.body_chat_main_container_chat_message_me}>{message}</Text></View>);
+    let AiraAnswer = AiraChooseAnswer(message);
 
-    function MainAndFooterButton(message){
-      sendMessage(message);
-    }
+    setMessages(prevState => [...prevState, Message, AiraAnswer]);
+    // setTextInput('');
+  }
 
+  function AiraChooseAnswer(MessageFromUser) {
+    let AiraAnswer = (<View style={styles.body_chat_main_container_message_aira}><Text style={styles.body_chat_main_container_chat_message_aira}>{dicionario['resposta'][MessageFromUser]}</Text></View>);
+    return AiraAnswer;
+  }
 
-
-
-  return(
+  function MainAndFooterButton(message) {
+    sendMessage(message);
+  }
+  return (
     <View style={styles.main}>
-      <Header/>
+      <Header />
       <View style={styles.body}>
-        
-          <View style={styles.messages_box}>
-            <ScrollView scrollEnabled={true} showsVerticalScrollIndicator={false}>
-                <View style={styles.messagesBox}>                  
-                  {messages.map((value,item)=> {
-                    return(
-                      <MessagesBox tkey={item}
-                      message={value}
-                      />
-                    )
-                  })}
-                </View>
-            </ScrollView>
-          </View>
+
+        <View style={styles.messages_box}>
+          <ScrollView scrollEnabled={true} showsVerticalScrollIndicator={false}>
+            <View style={styles.messagesBox}>
+              {messages.map((value, item) => {
+                return (
+                  <MessagesBox tkey={item}
+                    message={value}
+                  />
+                )
+              })}
+            </View>
+          </ScrollView>
+        </View>
 
       </View>
       <Footer MainAndFooterButton={MainAndFooterButton} />
@@ -96,59 +93,59 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
-  messages_box:{
+  messages_box: {
     height: vh(80),
     marginHorizontal: 10,
   },
-  messagesBox:{
+  messagesBox: {
     //backgroundColor: 'red',
-  },  
-  
+  },
+
   body_chat_main_container: {
     borderColor: 'black',
-    borderBottomWidth:1,
-    borderTopWidth:1, 
+    borderBottomWidth: 1,
+    borderTopWidth: 1,
     padding: 1,
-    margin: 20,      
+    margin: 20,
   },
   body_chat_main_container_message_aira: {
-    flex:1,
+    flex: 1,
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'flex-start'
   },
   body_chat_main_container_message_me: {
-    flex:1,
+    flex: 1,
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'flex-end',      
+    justifyContent: 'flex-end',
   },
   body_chat_main_container_chat_message_aira: {
     borderColor: '#aff',
     backgroundColor: '#aff',
-    width: '50%',     
-    fontSize: 12,        
+    width: '50%',
+    fontSize: 12,
     borderLeftWidth: 1,
     borderRightWidth: 1,
-    borderBottomWidth:1,
-    borderTopWidth:1, 
+    borderBottomWidth: 1,
+    borderTopWidth: 1,
     borderRadius: 20,
     padding: 16,
-    margin: 20,    
+    margin: 20,
   },
 
   body_chat_main_container_chat_message_me: {
     borderColor: '#fcf',
     backgroundColor: '#fcf',
-    width: '50%',     
-    fontSize: 12,       
+    width: '50%',
+    fontSize: 12,
     borderLeftWidth: 1,
     borderRightWidth: 1,
-    borderBottomWidth:1,
-    borderTopWidth:1, 
+    borderBottomWidth: 1,
+    borderTopWidth: 1,
     borderRadius: 20,
     padding: 16,
-    margin: 20,            
+    margin: 20,
   },
 
   footer: {
@@ -173,8 +170,8 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     padding: 15,
     width: '55%',
-  },  
-  footer_menu_button_record: { 
+  },
+  footer_menu_button_record: {
     padding: 7,
     height: 40,
     borderRadius: 20,
@@ -184,12 +181,12 @@ const styles = StyleSheet.create({
 
   },
   footer_menu_button_record_icon: {
-    flex: 1,      
+    flex: 1,
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-around'
   },
-  footer_menu_button_send: { 
+  footer_menu_button_send: {
     padding: 7,
     height: 40,
     borderRadius: 20,
